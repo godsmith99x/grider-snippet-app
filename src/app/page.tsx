f9f8e10fa794c504b5badfaa0f44f3bd900043b3
godsmith <j.godsmith@proton.me>
2024-01-18 18:07:29 -0500

    Styled Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
 import { db } from "@/db";
+import Link from "next/link";
 
 export default async function Home() {
   const snippets = await db.snippets.findMany();
 
   const renderedSnippets = snippets.map((snippet) => {
-    return <div key={snippet.id}>{snippet.title}</div>;
+    return (
+      <Link
+        key={snippet.id}
+        href={`/snippets/${snippet.id}`}
+        className="flex justify-between item-center p-2 border rounded border-gray-200 hover:bg-gray-100"
+      >
+        <div>{snippet.title}</div>
+        <div>View</div>
+      </Link>
+    );
   });
 
-  return <div>{renderedSnippets}</div>;
+  return (
+    <div>
+      <div className="flex m-2 justify-between items-center">
+        <h1 className="text-xl font-bold">Snippets</h1>
+        <Link className="border rounded p-2" href={`/snippets/new`}>
+          New
+        </Link>
+      </div>
+      <div className="flex flex-col gap-2">{renderedSnippets}</div>
+    </div>
+  );
 }
